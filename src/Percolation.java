5cc2e8bb7f0842a3c6c256138a76c1192aaea2d5
coco11563 <coco11563@yeah.net>
2016-09-06 13:54:28 +0800

    完成了第一週的編程作業

--- src/Percolation.java
+++ src/Percolation.java
@@ -1,139 +1,83 @@
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
 public class Percolation {
-	private int[] metallic;
-	private int[] sz;
-	private int id[];
-	private int count;
-	private int n;
-	private int[] percolation;
-	public Percolation(int n) {// create n-by-n grid, with all sites blocked
-		// TODO Auto-generated constructor stub
-		int num = Integer.valueOf(n);
-		if (n < 1) throw new IllegalArgumentException("Illeagal Argument");
-		this.n = num;
-		int size = num * num;
-		metallic = new int[size + 2];
-		sz = new int [size + 2];
-		id = new int [size + 2];
-		percolation = new int [num];
-		count = size + 2;
-		for (int i = 0; i < size + 2; i++) {
-			metallic[i] = 0;
-			id[i] = i;
-			sz[i] = 1;
-		}
-		for (int i = 0 ; i < num ; i++) {
-			percolation[i] = 0;
-		}
-	}
-
-
-	public boolean isFull(int row, int col) {// is site (row i, column j) full?
-		// TODO Auto-generated method stub
-		if (row == n) {
-			if (percolation[col] != 1) {
-				if (checkPer(row , col)) {
-					return true;
-				}
-			}
-		}
-		else if (connected(toNum(row, col), 0)) {
-			return true;
-		}
-		return false;
-	} 
-	
-	private boolean checkPer(int i , int j) {
-		if ((i - 1) > 0 && connected(toNum(i - 1, j) , 0)) {
-			return true; 
-		}if ((j - 1) > 0 && connected(toNum(i, j - 1) , 0)) {
-			return true;
-		} if ((j + 1) > 0 && connected(toNum(i, j + 1) , 0)) {
-			return true;
-		}
-		return false;
-	}
-	public boolean percolates() { // does the system percolate?
-		// TODO Auto-generated method stub
-		if (connected(0, count - 1)) {
-			return true;
-		} else {
-		return false;
-		}
-	}
-
-	public boolean isOpen(int row, int col) {// is site (row i, column j) open?
-		// TODO Auto-generated method stub
-		if (metallic[toNum(row, col)] == 1) {
-			return true;
-		}
-			return false;
-		
-	}
-	private int toNum(int row, int col) {
-		 if (row <= 0 || row > n) 
-			 throw new IndexOutOfBoundsException("row i out of bound");
-	     if (col <= 0 || col > n) 
-	    	 throw new IndexOutOfBoundsException("column j out of bound");
-		return (row - 1) * n + col;
-	}
-	public void open(int i, int j) {// open site (row i, column j) if  it is not open already
-		// TODO Auto-generated method stub
-		if (!isOpen(i, j)) { 
-			metallic[toNum(i, j)] = 1;
-		if (i == 1) {
-			checkNearby(i, j);
-			union(0, toNum(i, j));
-		}
-		if (i == n) {
-			checkNearby(i, j);
-			union(count - 1, toNum(i, j));
-			percolation[j] = 1;
-		} else {
-			checkNearby(i, j);
-		}
-		}
-	}
-		
-	private void checkNearby(int i, int j) {
-		if ((i -1) > 0 && isOpen(i-1, j)) {
-			union(toNum(i-1, j), toNum(i, j)); 
-		} if ((i+1) <= n && isOpen(i+1, j)) {
-			union(toNum(i+1, j), toNum(i, j)); 
-		} if ((j - 1) > 0 && isOpen(i, j-1)) {
-			union(toNum(i, j-1), toNum(i, j)); 
-		} if ((j + 1) <= n && isOpen(i, j+1)) {
-			union(toNum(i, j+1), toNum(i, j)); 
-		}
-	}
-
-	private int find(int num) {
-		while(num != id[num]) {
-		id[num] = id[id[num]]; 
-			num = id[num];
-		}
-		return num;
-	}
-	private void union(int num1, int num2) {
-		int pid = find(num1); 
-		int qid = find(num2);
-		if (sz[pid] < sz[qid])
-			{
-				id[pid] = qid; 
-				sz[qid] = sz[qid] + sz[pid];
-			}
-		else {
-			id[qid] = pid; 
-			sz[pid] = sz[qid] + sz[pid];
-		}
-	}
-	private boolean connected(int num1, int num2) {
-		if (find(num1) == find(num2)) {
-			return true;
-		}
-		else {
-			return false;
-		}
-	}
-	public static void main(String args[]) {// test client (optional)
-	}
+    
+    private boolean[] matrix;
+    private int row, col;
+    private WeightedQuickUnionUF wquUF;
+    private WeightedQuickUnionUF wquUFTop;
+    private boolean alreadyPercolates;
+    
+    public Percolation(int N) {
+        if (N < 1) throw new IllegalArgumentException("Illeagal Argument");
+        wquUF = new WeightedQuickUnionUF(N*N+2);
+        wquUFTop = new WeightedQuickUnionUF(N*N+1);
+        alreadyPercolates = false;
+        row = N;
+        col = N;
+        matrix = new boolean[N*N+1];
+    }
+    
+    private void validate(int i, int j) {
+        if (i < 1 || i > row) 
+            throw new IndexOutOfBoundsException("row index i out of bounds");
+        if (j < 1 || j > col) 
+            throw new IndexOutOfBoundsException("col index j out of bounds");        
+    }
+    
+    public void open(int i, int j) {
+        validate(i, j);
+        int curIdx = (i-1)*col + j; 
+        matrix[curIdx] = true;
+        if (i == 1) {
+            wquUF.union(curIdx, 0);
+            wquUFTop.union(curIdx, 0);
+        }
+        if (i == row) {
+            wquUF.union(curIdx, row*col+1);
+        }
+        
+        int[] dx = {1, -1, 0, 0};
+        int[] dy = {0, 0, 1, -1};
+        for (int dir = 0; dir < 4; dir++) {
+            int posX = i + dx[dir];
+            int posY = j + dy[dir];
+            if (posX <= row && posX >= 1 
+                    && posY <= row && posY >= 1 
+                    && isOpen(posX, posY)) {
+                wquUF.union(curIdx, (posX-1)*col+posY);
+                wquUFTop.union(curIdx, (posX-1)*col+posY);
+            }
+        }
+    }
+    
+    public boolean isOpen(int i, int j) {
+        validate(i, j);
+        return matrix[(i-1)*col + j];
+    }
+    
+    public boolean isFull(int i, int j) {
+        validate(i, j);
+        int curIdx = (i-1)*col+j;
+        if (wquUFTop.find(curIdx) == wquUFTop.find(0)) return true;
+        return false;
+    }
+    
+    public boolean percolates() {
+        if (alreadyPercolates) return true;
+        if (wquUF.find(0) == wquUF.find(row*col+1)) {
+            alreadyPercolates = true;
+            return true;
+        } 
+        return false;
+    }
+    
+    public static void main(String[] args) {
+        Percolation perc = new Percolation(2);
+        perc.open(1, 1);
+        perc.open(1, 2);
+        perc.open(2, 1);
+        System.out.println(perc.percolates());
+    }
+    
 }
--- src/Week_One_Home_Work/Percolation.java
+++ src/Week_One_Home_Work/Percolation.java
@@ -1,107 +1,147 @@
 package Week_One_Home_Work;
 
 public class Percolation {
-	private int[] metallic ;
-	private int[] sz ;
-	int id[] ;
-	int count ;
-	int n ;
+	private int[] metallic;
+	private int[] sz;
+	private int id[];
+	private int count;
+	private int n;
+	private int[] percolation;
 	public Percolation(int n) {// create n-by-n grid, with all sites blocked
 		// TODO Auto-generated constructor stub
-		this.n = new Integer(n) ;
-		n = n * n;
-		metallic = new int[n + 2] ;
-		sz = new int [n + 2] ;
-		id = new int [n + 2] ;
-		count = n + 2 ;
-		for(int i = 0 ; i < n + 2 ; i++){//n+1和0点
-			metallic[i] = 0 ;
-			id[i] = i ;
-			sz[i] = 1 ;
+		int num = Integer.valueOf(n);
+		if (n < 1) throw new IllegalArgumentException("Illeagal Argument");
+		this.n = num;
+		int size = num * num;
+		metallic = new int[size + 2];
+		sz = new int [size + 2];
+		id = new int [size + 2];
+		percolation = new int [num];
+		count = size + 2;
+		for (int i = 0; i < size + 2; i++) {
+			metallic[i] = 0;
+			id[i] = i;
+			sz[i] = 1;
+		}
+		for (int i = 0 ; i < num ; i++) {
+			percolation[i] = 0;
 		}
 	}
-	
+
 
 	public boolean isFull(int row, int col) {// is site (row i, column j) full?
 		// TODO Auto-generated method stub
-		if (connected(toNum(row, col), 0)) {
+		if (row == n) {
+			if (percolation[col - 1] != 1){
+				if(isOpen(row , col) && checkPer(row , col)) {	
+					percolation[col - 1] = 1;
+					return true;
+				} 
+			}
+			else {
+				return true;
+			}
+		}
+		else if (connected(toNum(row, col), 0)) {
 			return true;
 		}
 		return false;
 	} 
-
+	
+	private boolean checkPer(int i , int j) {
+		if ((i - 1) > 0 && isOpen(i - 1 , j) && connected(toNum(i - 1, j) , 0)) {
+			return true; 
+		}if ((j - 1) > 0 && isOpen(i , j - 1) && percolation[j - 1] == 1) {
+			return true;
+		} if ((j + 1) < n && isOpen(i , j + 1) && percolation[j + 1] == 1) {
+			return true;
+		}
+		return false;
+	}
 	public boolean percolates() { // does the system percolate?
 		// TODO Auto-generated method stub
-		if(connected(0 , count - 1)){
-			return true ;
-		}else{
+		if (connected(0, count - 1)) {
+			return true;
+		} else {
 		return false;
 		}
 	}
 
 	public boolean isOpen(int row, int col) {// is site (row i, column j) open?
 		// TODO Auto-generated method stub
-		if(metallic[toNum(row , col)] == 1){
-			return true ;
+		if (metallic[toNum(row, col)] == 1) {
+			return true;
 		}
-		return false ;
-	}
-	public int toNum(int row , int col){
-		return ( row - 1 ) * n + col ;
-	}
-	public void open(int i, int j) {// open site (row i, column j) if it is not open already
-		// TODO Auto-generated method stub
-		if(!isOpen(i , j)){ //这个点没开
-			metallic[toNum(i , j)] = 1 ;
-			if(i == 1){
-				checkNearby(i , j);
-				union(0 , toNum(i , j));
-			}if(i == n){
-				checkNearby(i , j);
-				union(count - 1 , toNum(i , j));
-			}else{
-				checkNearby(i , j);
-			}
-			
-			
-		}
+			return false;
 		
 	}
-	public void checkNearby(int i , int j){
-		if((i -1) > 0&&isOpen(i-1 , j)){//上面的是开的
-			union(toNum(i-1 , j) , toNum(i , j)); //就把上面的根与这个点相连
-		}if((i+1) <= n&&isOpen(i+1 , j)){//下面的是开的
-			union(toNum(i+1 , j) , toNum(i , j)); //就把下面的根与这个点相连
-		}if((j - 1) > 0&& isOpen(i , j-1) ){//左面的是开的
-			union(toNum(i , j-1) , toNum(i , j)); //就把左边的根与这个点相连
-		}if((j + 1) <= n&&isOpen(i , j+1) ){//右面的是开的
-			union(toNum(i , j+1) , toNum(i , j)); //就把右边的根与这个点相连
+	private int toNum(int row, int col) {
+		 if (row <= 0 || row > n) 
+			 throw new IndexOutOfBoundsException("row i out of bound");
+	     if (col <= 0 || col > n) 
+	    	 throw new IndexOutOfBoundsException("column j out of bound");
+		return (row - 1) * n + col;
+	}
+	public void open(int i, int j) {// open site (row i, column j) if  it is not open already
+		// TODO Auto-generated method stub
+		if (!isOpen(i, j)) { 
+			metallic[toNum(i, j)] = 1;
+		if (i == 1) {
+			checkNearby(i, j);
+			union(0, toNum(i, j));
+		}
+		if (i == n) {
+			if(checkPer(i , j)){
+				percolation[j - 1] = 1;
+			}
+			checkNearby(i, j);
+			union(count - 1, toNum(i, j));
+		} else {
+			checkNearby(i, j);
+		}
+		}
+	}
+		
+	private void checkNearby(int i, int j) {
+		if ((i -1) > 0 && isOpen(i-1, j)) {
+			union(toNum(i-1, j), toNum(i, j)); 
+		} if ((i+1) <= n && isOpen(i+1, j)) {
+			union(toNum(i+1, j), toNum(i, j)); 
+		} if ((j - 1) > 0 && isOpen(i, j-1)) {
+			union(toNum(i, j-1), toNum(i, j)); 
+		} if ((j + 1) <= n && isOpen(i, j+1)) {
+			union(toNum(i, j+1), toNum(i, j)); 
 		}
 	}
 
-	public int getRoot(int num){
-		while(num!=id[num]){
-//			id[num] = id[id[num]]; //如果每次都是根连根或者二级连二级那么路径压缩就没有意义了
+	private int find(int num) {
+		while(num != id[num]) {
+		id[num] = id[id[num]]; 
 			num = id[num];
 		}
-		return num ;
+		return num;
 	}
-	public void union(int num1 , int num2){
-		int pid = getRoot(num1) ; 
-		int qid = getRoot(num2) ;
-		if(sz[pid] < sz[qid]){id[pid] = qid ; sz[qid] = sz[qid] + sz[pid];}//pid树小则把小树连到大树上
-		else{id[qid] = pid ; sz[pid] = sz[qid] + sz[pid];}
+	private void union(int num1, int num2) {
+		int pid = find(num1); 
+		int qid = find(num2);
+		if (sz[pid] < sz[qid])
+			{
+				id[pid] = qid; 
+				sz[qid] = sz[qid] + sz[pid];
+			}
+		else {
+			id[qid] = pid; 
+			sz[pid] = sz[qid] + sz[pid];
+		}
 	}
-	public boolean connected(int num1 , int num2){
-		if(getRoot(num1) == getRoot(num2)){
+	private boolean connected(int num1, int num2) {
+		if (find(num1) == find(num2)) {
 			return true;
 		}
 		else {
 			return false;
 		}
 	}
-	public static void main(String args[]){// test client (optional)
-		Percolation per = new Percolation(10);
-		PercolationVisualizer.draw(per,10);
+	public static void main(String args[]) {// test client (optional)
 	}
 }
