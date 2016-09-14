f98694d2d650493d52e7924741cf0706039115f8
coco11563 <coco11563@yeah.net>
2016-09-14 20:18:32 +0800

    存盘，明日再战！

--- src/Week_Two_Home_Work/RandomizedQueue.java
+++ src/Week_Two_Home_Work/RandomizedQueue.java
@@ -1,29 +1,62 @@
 package Week_Two_Home_Work;
 
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 /**
  * Created by coco1 on 2016/9/14.
  */
 public class RandomizedQueue<T> extends AbstructRandomizedQueue<T> {
+
+    private int count;
+    private Node front;//insert queue point
+    private Node last;//out queue point
+    private class Node {
+        T t;
+        Node next;  //out
+        Node prior; //inset
+
+        Node(T item) {
+            t = item;
+            next = null;
+            prior = null;
+        }
+    }
     @Override
     public boolean isEmpty() {
-        return false;
+        return count == 0;
     }
 
     @Override
     public int size() {
-        return 0;
+        return count;
     }
 
     @Override
     public void enqueue(T t) {
-
+        if (t == null) {
+            throw new NullPointerException("Try to enqueue a null item");
+        }
+        if (isEmpty()) {//empty
+            front = last = new Node(t);
+            count ++;
+        } else {
+            Node old_front = front;
+            front.prior = new Node(t);
+            front = front.prior;
+            front.next = old_front;
+            count ++;
+        }
     }
 
     @Override
     public T dequeue() {
-        return null;
+        if (isEmpty()) {
+            throw new NoSuchElementException("Try to dequeue from a empty queue");
+        }
+        T t = front.t;
+        front = front.next;
+        return t;
     }
 
     @Override
@@ -34,5 +67,26 @@ public class RandomizedQueue<T> extends AbstructRandomizedQueue<T> {
     @Override
     public Iterator<T> iterator() {
         return null;
+    }
+
+    private class iterator implements Iterator<T>{
+        private Node iter;
+        @Override
+        public boolean hasNext() {
+            return iter != null;
+        }
+
+        @Override
+        public T next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException("No more element in the Queue!");
+            }
+            return null;
+        }
+
+        @Override
+        public void remove() {
+            throw new UnsupportedOperationException("Don't use this function!");
+        }
     }
 }
