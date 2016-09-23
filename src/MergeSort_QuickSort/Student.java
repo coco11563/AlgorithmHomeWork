b7e801b5da6216303fa89c7adb39da35f58ac888
coco11563 <coco11563@yeah.net>
2016-09-23 19:51:48 +0800

    Adding week 2 homework data

---
+++ src/MergeSort_QuickSort/Student.java
@@ -0,0 +1,31 @@
+package MergeSort_QuickSort;
+
+import java.util.Comparator;
+
+/**
+ * Created by coco1 on 2016/9/23.
+ *
+ * A standard example for Comparator in use
+ */
+public class Student {
+    public String name;
+    public int age;
+
+    //declare comparator
+    public static final Comparator<Student> BYNAME = new ByName();
+    public static final Comparator<Student> BYAGE = new ByAge();
+
+    private static class ByName implements Comparator<Student> {
+        @Override
+        public int compare(Student o1, Student o2) {
+            return o1.name.compareTo(o2.name);//use String way to sort
+        }
+    }
+    private static class ByAge implements Comparator<Student> {
+
+        @Override
+        public int compare(Student o1, Student o2) {
+            return o1.age - o2.age;//use int way to sort
+        }
+    }
+}
