804b0f3588ae9ccee3ed828613de9ae8c9a73369
coco11563 <coco11563@yeah.net>
2016-09-28 18:48:09 +0800

    Update package-Info

---
+++ src/MergeSort_QuickSort/package-info.java
@@ -0,0 +1,13 @@
+/**
+ * Created by coco1 on 2016/9/28.
+ *
+ * when we talk about Arrays.sort(java.util.*)
+ *
+ * it is a method for data types that implement Comparable or has a method use Comparator
+ *
+ * Use quick-sort for primitive types <---- stable and guarantees n*log(n)
+ * Use merge-sort for objects <----quick but not stable, use less memory
+ *
+ *
+ */
+package MergeSort_QuickSort;
