ce34105192c10d3c5b9dc80c1ee6a5b64575671f
coco11563 <coco11563@yeah.net>
2016-12-15 14:45:58 +0800

    扫描线算法

---
+++ src/GeoMetricApplications/LineSegmentIntersectionVerticalCase.java
@@ -0,0 +1,18 @@
+package GeoMetricApplications;
+
+/**
+ * Created by coco1 on 2016/12/15.
+ *
+ * sweep ***vertical*** line from left to right
+ *
+ * x-coordinates define events
+ *
+ * h-segment(left endpoint) insert y-coordinate into BST
+ *
+ * h-segment(right endpoint) remove y-coordinate from BST
+ *
+ * v-segment:range search for interval of y-endpoint
+ */
+public class LineSegmentIntersectionVerticalCase {
+
+}
